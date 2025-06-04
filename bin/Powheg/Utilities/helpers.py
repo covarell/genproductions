40d0125ad178f09dc3f3343bcf1e4c5e92a7ac8a
Roberto Covarelli <roberto.covarelli@cern.ch>
2025-06-04 11:21:33 +0200

    start adding gg4l

--- bin/Powheg/Utilities/helpers.py
+++ bin/Powheg/Utilities/helpers.py
@@ -185,6 +185,13 @@ def runGetSource_patch_6(process) :
     "WWJ" : "cp Makefile Makefile.orig\n \
 cat Makefile.orig | sed -e \"s#FASTJET_CONFIG=.\+#FASTJET_CONFIG=$(scram tool info fastjet | grep BASE | cut -d \"=\" -f2)/bin/fastjet-config#g\" | sed -e \"s#\#\ FASTJET_CONFIG#FASTJET_CONFIG#g\" | sed -e \"s#\#\ LIBSFASTJET#LIBSFASTJET#g\" | sed -e \"s#\#\ FJCXXFLAGS#FJCXXFLAGS#g\" > Makefile\n \
 cd ${WORKDIR}/${name}/POWHEG-BOX/MATRIXStuff\n \
+sed -i -e 's#python#python3#g' matrix\n \
+sed -i -e 's/print /print ("") # print /g' matrix\n \
+sed -i -e 's#python#python3#g' bin/*\n \
+sed -i -e 's/print /print("") # print /g' bin/*\n \
+sed -i -e 's#python#python3#g' bin/modules/*\n \
+sed -i -e 's/print /print("") # print /g' bin/modules/*\n \
+./matrix --minnlo_interface\n \
 cd -\n \
 cd ${WORKDIR}/${name}\n \
 ln -s /cvmfs/cms-griddata.cern.ch/gen/Run2UL_powheg_v2/WW_MiNNLO_2loop_grids_reduced1/TWOLOOP_GRIDS_reg1 TWOLOOP_GRIDS_reg1\n \
