560b40fa24a29b64ed36c0a0d87e1e92af8682e8
Roberto Covarelli <roberto.covarelli@cern.ch>
2025-10-14 13:43:09 +0200

    one more change

--- bin/Powheg/Templates/createTarBall_template.sh
+++ bin/Powheg/Templates/createTarBall_template.sh
@@ -94,6 +94,7 @@ cp -p $${WORKDIR}/$$folderName/POWHEG-BOX/*tuff*/external/*amp*/*.so* ./lib
 cp -p $${WORKDIR}/$$folderName/POWHEG-BOX/$${process}/*/lib/*.so* ./lib
 cp -p $${WORKDIR}/$$folderName/POWHEG-BOX/$${process}/*/lib/*.a ./lib
 cp -r $${WORKDIR}/$$folderName/POWHEG-BOX/$${process}/obj-gfortran . 
+cp -pr $${WORKDIR}/$$folderName/POWHEG-BOX/OpenLoopsStuff/OpenLoop*/obj-gfortran/* ./obj-gfortran
 
 sed -i 's/pwggrid.dat ]]/pwggrid.dat ]] || [ -e $${WORKDIR}\/pwggrid-0001.dat ]/g' runcmsgrid.sh
 
